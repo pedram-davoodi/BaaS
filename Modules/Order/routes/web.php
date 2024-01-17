9a8c5e0c3c14813c871d017e1f8ef0339d389aba
Pedram Davoodi <pedramdavoodi1376@gmail.com>
2024-01-17 23:13:42 +0330

    improvment

--- Modules/Order/routes/web.php
+++ Modules/Order/routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Modules\Order\app\Http\Controllers\OrderController;
 
 /*
 |--------------------------------------------------------------------------
