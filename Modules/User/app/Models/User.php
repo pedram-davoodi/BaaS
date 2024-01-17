4415ec829045b56e61358bef318b8884cd9c3af0
Pedram Davoodi <pedramdavoodi1376@gmail.com>
2024-01-17 22:52:11 +0330

    Fix test

--- Modules/User/app/Models/User.php
+++ Modules/User/app/Models/User.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
-use Modules\User\Database\Factories\UserFactory;
+use Modules\User\database\factories\UserFactory;
 
 class User extends Authenticatable implements UserModelInterface
 {
