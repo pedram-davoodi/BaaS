65cf4dd474dcf06251fc29509b4b74f261f6ba95
Pedram Davoodi <pedramdavoodi1376@gmail.com>
2024-10-11 11:17:11 +0330

    cart module added

---
+++ Modules/Cart/database/seeders/CartDatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\Cart\database\seeders;
+
+use Illuminate\Database\Seeder;
+
+class CartDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $this->call([]);
+    }
+}
---
+++ Modules/Cart/resources/views/index.blade.php
@@ -0,0 +1,7 @@
+@extends('cart::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>Module: {!! config('cart.name') !!}</p>
+@endsection
