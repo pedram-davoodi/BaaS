f47dd082c6ba9ef7f7e55182098f4bf18645001e
Pedram Davoodi <pedramdavoodi1376@gmail.com>
2024-10-12 18:17:10 +0330

    add a TODO

--- Modules/Cart/app/Services/CartService.php
+++ Modules/Cart/app/Services/CartService.php
@@ -9,6 +9,7 @@ class CartService
 {
     public function addNewItem(int $cartable_id, string $cartable_type, int $quantity)
     {
+        //TODO: new items those added before should increase quantity not add as a new item
         $repository = app(CartRepositoryInterface::class);
         $cart = $repository->getFirstWhere(['user_id' => Auth::id()]);
 
@@ -20,7 +21,7 @@ class CartService
         if ($cart?->items){
             $items = array_merge($items, json_decode($cart->items, true));
         }
-        
+
         return $repository->updateOrCreate(
             [
                 'user_id' => Auth::id()
